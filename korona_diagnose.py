176f8e208fab9dfa2eee8f049e2b18575b6b441b
null <somebody@domain.com>
2020-04-16 12:39:46 +0430

    Add try-except to handle errors.

--- korona_diagnose.py
+++ korona_diagnose.py
@@ -3,10 +3,13 @@ KORONA_TEMPRETURE = 37.5
 temp = int(input("Enter Your Body Tempreture: "))
 
 def tab_sanj():
-	if temp < KORONA_TEMPRETURE:
-		print("You are in safe range")
-	else:
-		print("Danger! Visit a doctor")
+	try:
+		if temp < KORONA_TEMPRETURE:
+			print("You are in safe range.")
+		else:
+			print("Danger! Visit a docter.")
+	except ValueError:
+		print("Wrong value entered. Try again later.")
 
 
 if __name__ =' __main__':
